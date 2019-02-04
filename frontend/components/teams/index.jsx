fbae83435fdcda8214c22818f364a37805001b63
Soon-Mi Sugihara <ssoonmi@gmail.com>
2019-02-04 16:51:05 -0500

    Initial frontend

---
+++ frontend/components/teams/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class TeamIndex extends React.Component {
+  render() {
+    return (
+      <h1>Hello</h1>
+    );
+  }
+}
+
+export default TeamIndex;
