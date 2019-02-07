26951b4e59cc55930a63fd31171ac0b8904f15b4
Brian Lee <ibltsandwich@gmail.com>
2019-02-07 04:25:37 -0500

    Styling for login and signup form/screen

--- src/components/session/session_form.jsx
+++ src/components/session/session_form.jsx
@@ -18,6 +18,13 @@ class SessionForm extends React.Component {
   }
 
   updateField(field) {
+    if (this.sessionSubmit){
+      if (this.state.username.length > 0 && this.state.password.length > 0) {
+        this.sessionSubmit.disabled = false;
+      } else {
+        this.sessionSubmit.disabled = true;
+      }
+    }
     return (e) => {
       this.setState({[field]: e.target.value});
     };
@@ -26,23 +33,25 @@ class SessionForm extends React.Component {
   render() {
     const isLogin = this.props.formType == 'login';
     return (
-      <article>
-        <h2>{isLogin ? "Log In" : "Sign Up"}</h2>
-        <form onSubmit={this.handleSubmit()}>
-          <label>
-            Username: 
-            <input type="text" value={this.state.username} onChange={this.updateField("username")}/>
-          </label>
-          <label>
-            Password: 
-            <input type="password" value={this.state.password} onChange={this.updateField("password")} />
-          </label>
-          <input type="submit" value={isLogin ? "Log In" : "Sign Up"}/>
-        </form>
-        {isLogin ? 
-          <Link to="/signup">Don't have an account? Sign Up</Link> : <Link to="/login">Already a user? Log In</Link>
-        }
-      </article>
+      <div className="session-screen">
+        <article className="session-form-container">
+          <h2>{isLogin ? "Log In" : "Sign Up"}</h2>
+          <form onSubmit={this.handleSubmit()} className="session-form">
+            <label htmlFor="username">
+              Username: 
+              <input id="username" type="text" value={this.state.username} onChange={this.updateField("username")} className="session-form-info"/>
+            </label>
+            <label htmlFor="password">
+              Password: 
+              <input id="password" type="password" value={this.state.password} onChange={this.updateField("password")} className="session-form-info"/>
+            </label>
+            <input type="submit" value={isLogin ? "Log In" : "Sign Up"} ref={elem => this.sessionSubmit = elem} className="session-form-submit" disabled/>
+          </form>
+          {isLogin ? 
+            <Link to="/signup">Don't have an account? Sign Up</Link> : <Link to="/login">Already a user? Log In</Link>
+          }
+        </article>
+      </div>
     );
   }
 }
