045615891a4f39b21878142bda8a81f3bd7d363c
Brian Lee <ibltsandwich@gmail.com>
2019-03-19 20:04:28 -0400

    Add team roster to team show page

--- src/components/teams/show.jsx
+++ src/components/teams/show.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { fetchPlayers } from '../../actions/team_actions';
 
 class TeamShow extends React.Component {
   render() {
     if (!this.props.teamLoading) {
       const { team } = this.props;
+      const roles = {midlane: "Mid Lane", 
+                     toplane: "Top Lane", 
+                     jungle: "Jungle",
+                     support: "Support",
+                     adcarry: "Bot Lane"}
+
+      const players = team.players.map((player, idx) => {
+        const socials = [];
+        for (const platform in socialNetworks) {
+          socials.push(<li>
+            <a href={socialNetworks.platform}>{platform[0].toUppercase + platform.slice(1)}</a>
+          </li>)
+        };
+
+        return <li key={idx} className="player-list-item">
+          <h1>{player.name}</h1>
+          <img src={player.photoUrl}/>
+          <h3>{player.firstName} {player.lastName}</h3>
+          <h3 className="player-role">{roles[player.roleSlug]}</h3>
+          <ul>{socials}</ul>
+        </li>
+      })
+
       return (
         <section className="team-show">
           <div>
-            <h2>{team.name}</h2>
+            <header className="team-show-header">
+              <img className="team-logo" src={team.logoUrl}/>
+              <h2>{team.name}</h2>
+            </header>
+            <section className="team-show-roster">
+              <ul>
+                {players}
+              </ul>
+            </section>
           </div>
         </section>
       );
