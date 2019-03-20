81fcabf76619cdd9af070b48cdffacf144d9ebc1
Brian Lee <ibltsandwich@gmail.com>
2019-03-20 17:53:00 -0400

    styling for team index

--- src/components/teams/index.jsx
+++ src/components/teams/index.jsx
@@ -5,24 +5,61 @@ import { connect } from 'react-redux';
 import TeamShow from './show';
 
 class TeamIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.zoomIn = this.zoomIn.bind(this);
+  }
+
   componentDidMount() {
     if (!this.props.fetchedTeams) {
       this.props.fetchTeams();
     }
   }
 
+  // zoomIn(e) {
+  //   e.currentTarget.backgroundSize = `90%`;
+  //   // if (this.teamListItem) {
+  //   //   this.teamListItem.backgroundSize = `90%`;
+  //   // }
+  // }
+
   render() {
     
     const { teams, teamName } = this.props;
     let teamId;
+    let teamLogos = {};
+
+    if (teams[0].teams) {
+      teams[0].teams.forEach(team => {
+        teamLogos[team.id] = team.teamPhotoUrl
+      })
+    }
     const teamLis = teams.map((team, idx) => {
       if (teamName == team.acronym) {
         teamId = team.team;
       }
+
+      const background = team.logoUrl;
+      const style = { background: `url(${background})`, 
+                      backgroundPositionY: `center`, 
+                      backgroundRepeat: `no-repeat`, 
+                      backgroundSize: `70%`,
+                      position: `relative` };
+
       return (
-        <li key={idx}><NavLink to={`/teams/${team.acronym}`}>{team.acronym}</NavLink></li>
+        <li onHover={this.zoomIn} 
+            className="team-list-item" 
+            key={idx} 
+            ref={el => (this.teamListItem = el)} 
+            style={style}>
+          <div className="team-gradient"></div>
+          <NavLink to={`/teams/${team.acronym}`}>{team.acronym}</NavLink>
+        </li>
       );
     });
+
+
+
     return (
       <article className="team-index">
         <section className="team-index-list">
